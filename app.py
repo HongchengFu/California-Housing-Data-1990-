b0466023cdc07f17711d8574c18b28dadfd9283a
HongchengFu <148854483+HongchengFu@users.noreply.github.com>
2024-09-23 21:56:08 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,50 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set()
+
+
+st.title('California Housing Data(1990)')
+df = pd.read_csv('housing.csv')
+
+
+house_price_filter = st.slider('Minimal Median House Pirce :', 0, 500001, 200000)  
+st.header('See more filters in the sidebar:')
+
+choose_the_location_type = st.sidebar.multiselect(
+     'Choose the loctaion type',
+     df.ocean_proximity.unique(),  
+     df.ocean_proximity.unique())  
+
+income_level =st.sidebar.radio(
+    "Select income level:",
+    ('Low','Meidum','High'))
+
+if income_level =='Low':
+    filtered_df = df[df.median_income <=2.5]
+elif income_level =='Meidum':
+    filtered_df = df[(df.median_income >2.5)&(df.median_income <4.5)]
+else :
+    filtered_df = df[df.median_income >4.5]    
+
+# show on map
+st.map(df)
+
+# show the plot
+st.subheader('The Median House Value')
+fig, ax = plt.subplots(figsize=(20, 5))
+
+
+median_income = df.median_house_value.value_counts()
+df.median_income.hist(bins=30)
+ 
+
+fig, ax = plt.subplots(figsize=(15, 8))
+data =df[(df.median_house_value>=200000)&(df.median_income<=2.5)]
+
+data.median_house_value.hist(bins=30)
+
+
+plt.grid(True)
+st.pyplot(fig)
